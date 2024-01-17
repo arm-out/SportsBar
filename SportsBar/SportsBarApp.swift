65367bf256b5afa27e2080a9f1f43e83047b4aa0
arm-out <arminsuraj@gmail.com>
2024-01-17 14:16:34 -0800

    Initial Commit

---
+++ SportsBar/SportsBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportsBarApp.swift
+//  SportsBar
+//
+//  Created by Armin Suraj on 1/17/24.
+//
+
+import SwiftUI
+
+@main
+struct SportsBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
